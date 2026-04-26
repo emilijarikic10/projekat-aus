a177c5a49ff405bdcc45116aae88aaad9729c888
emilijarikic10 <emilijarikic10@gmail.com>
2026-04-26 21:50:22 +0200

    RtuCfg.txt file modification

--- dCom/ProcessingModule/Acquisitor.cs
+++ dCom/ProcessingModule/Acquisitor.cs
@@ -8,12 +8,12 @@ namespace ProcessingModule
     /// Class containing logic for periodic polling.
     /// </summary>
     public class Acquisitor : IDisposable
-	{
-		private AutoResetEvent acquisitionTrigger;
+    {
+        private AutoResetEvent acquisitionTrigger;
         private IProcessingManager processingManager;
         private Thread acquisitionWorker;
-		private IStateUpdater stateUpdater;
-		private IConfiguration configuration;
+        private IStateUpdater stateUpdater;
+        private IConfiguration configuration;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Acquisitor"/> class.
@@ -23,33 +23,33 @@ namespace ProcessingModule
         /// <param name="stateUpdater">The state updater.</param>
         /// <param name="configuration">The configuration.</param>
 		public Acquisitor(AutoResetEvent acquisitionTrigger, IProcessingManager processingManager, IStateUpdater stateUpdater, IConfiguration configuration)
-		{
-			this.stateUpdater = stateUpdater;
-			this.acquisitionTrigger = acquisitionTrigger;
-			this.processingManager = processingManager;
-			this.configuration = configuration;
-			this.InitializeAcquisitionThread();
-			this.StartAcquisitionThread();
-		}
+        {
+            this.stateUpdater = stateUpdater;
+            this.acquisitionTrigger = acquisitionTrigger;
+            this.processingManager = processingManager;
+            this.configuration = configuration;
+            this.InitializeAcquisitionThread();
+            this.StartAcquisitionThread();
+        }
 
-		#region Private Methods
+        #region Private Methods
 
         /// <summary>
         /// Initializes the acquisition thread.
         /// </summary>
-		private void InitializeAcquisitionThread()
-		{
-			this.acquisitionWorker = new Thread(Acquisition_DoWork);
-			this.acquisitionWorker.Name = "Acquisition thread";
-		}
+        private void InitializeAcquisitionThread()
+        {
+            this.acquisitionWorker = new Thread(Acquisition_DoWork);
+            this.acquisitionWorker.Name = "Acquisition thread";
+        }
 
         /// <summary>
         /// Starts the acquisition thread.
         /// </summary>
 		private void StartAcquisitionThread()
-		{
-			acquisitionWorker.Start();
-		}
+        {
+            acquisitionWorker.Start();
+        }
 
         /// <summary>
         /// Acquisitor thread logic.
@@ -64,24 +64,32 @@ namespace ProcessingModule
 
                 foreach (IConfigItem item in configuration.GetConfigurationItems())
                 {
-                    // analogne (3100 i 1500) - svake 3 sekunde
-                    if ((item.StartAddress == 3100 || item.StartAddress == 1500) && counter % 3 == 0)
+                    bool isAnalog =
+                        item.RegistryType == PointType.ANALOG_INPUT ||
+                        item.RegistryType == PointType.ANALOG_OUTPUT;
+
+                    bool isDigital =
+                        item.RegistryType == PointType.DIGITAL_INPUT ||
+                        item.RegistryType == PointType.DIGITAL_OUTPUT;
+
+                    // DIGITALNI → 4 sekunde
+                    if (isDigital && counter % 4 == 0)
                     {
                         processingManager.ExecuteReadCommand(
                             item,
-                            counter,
-                            99,
+                            configuration.GetTransactionId(),
+                            configuration.UnitAddress,
                             item.StartAddress,
                             item.NumberOfRegisters
                         );
                     }
-                    // digitalne (3400, 3402, 3405, 3406) - svake 4 sekunde
-                    else if ((item.StartAddress == 3400 || item.StartAddress == 3402 || item.StartAddress == 3405 || item.StartAddress == 3406) && counter % 4 == 0)
+                    // ANALOGNI → 3 sekunde
+                    else if (isAnalog && counter % 3 == 0)
                     {
                         processingManager.ExecuteReadCommand(
                             item,
-                            counter,
-                            99,
+                            configuration.GetTransactionId(),
+                            configuration.UnitAddress,
                             item.StartAddress,
                             item.NumberOfRegisters
                         );
@@ -96,8 +104,8 @@ namespace ProcessingModule
 
         /// <inheritdoc />
         public void Dispose()
-		{
-			acquisitionWorker.Abort();
+        {
+            acquisitionWorker.Abort();
         }
-	}
+    }
 }
